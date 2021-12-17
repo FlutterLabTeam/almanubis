91931b0425c36b54d4264f047cad224450e53311
jeisonVargas <jeison1vargas@gmail.com>
2021-12-17 17:16:02 -0500

    se realizo el input

---
+++ lib/core/components/input/custom_input.dart
@@ -0,0 +1,115 @@
+import 'package:almanubis/core/util/company_colors.dart';
+import 'package:flutter/material.dart';
+
+enum CustomInputColor {
+  light,
+  dark,
+}
+
+enum CustomInputType { number, string, password, email }
+
+class CustomInputColorModel {
+  final TextEditingController? controller;
+  final CustomInputType typeInput;
+  final CustomInputColor color;
+  final String label;
+  final bool isObscure;
+
+  CustomInputColorModel({
+    this.typeInput = CustomInputType.string,
+    this.color = CustomInputColor.light,
+    this.controller,
+    this.isObscure = false,
+    this.label = "",
+  });
+}
+
+class CustomInput extends StatelessWidget {
+  final CustomInputColorModel model;
+
+  const CustomInput({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: false,
+      obscureText: model.isObscure,
+      style: TextStyle(
+        fontSize: 17.0,
+        color: _getColor(model.color),
+        fontWeight: FontWeight.w300,
+      ),
+      cursorColor: _getColor(model.color),
+      keyboardType: _getTypeInput(model.typeInput),
+      decoration: InputDecoration(
+        suffixIcon:
+            _getTypeInput(model.typeInput) == TextInputType.visiblePassword
+                ? model.isObscure
+                    ? IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.remove_red_eye,
+                          color: _getColor(model.color),
+                        ),
+                      )
+                    : IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.visibility_off,
+                          color: _getColor(model.color),
+                        ),
+                      )
+                : null,
+        hintText: model.label,
+        hintStyle: TextStyle(
+          color: _getColor(model.color),
+          fontWeight: FontWeight.w300,
+          fontSize: 16,
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: _getColor(model.color), width: 2),
+        ),
+      ),
+    );
+  }
+
+  Color _getColor(CustomInputColor color) {
+    switch (color) {
+      case CustomInputColor.light:
+        return CompanyColor.color().second;
+      case CustomInputColor.dark:
+        return CompanyColor.color().primary;
+    }
+  }
+
+  TextInputType _getTypeInput(CustomInputType type) {
+    switch (type) {
+      case CustomInputType.number:
+        return TextInputType.phone;
+      case CustomInputType.email:
+        return TextInputType.emailAddress;
+      case CustomInputType.string:
+        return TextInputType.text;
+      case CustomInputType.password:
+        return TextInputType.visiblePassword;
+    }
+  }
+}
--- lib/features/auth/presentation/pages/auth.dart
+++ lib/features/auth/presentation/pages/auth.dart
@@ -1,7 +1,8 @@
-import 'package:almanubis/core/components/button/custom_button.dart';
-import 'package:almanubis/core/components/image/custom_image.dart';
-import 'package:almanubis/core/components/navigation/navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:almanubis/core/components/image/custom_image.dart';
+import 'package:almanubis/core/components/input/custom_input.dart';
+import 'package:almanubis/core/components/button/custom_button.dart';
+import 'package:almanubis/core/components/navigation/navigation_bar.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -14,50 +15,69 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: 69,
-          child: CustomNavigationBar(
-            model: CustomNavigationBarModel(),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(
+            height: 20,
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: 69,
-          child: CustomNavigationBar(
-              model: CustomNavigationBarModel(),
-              onTapPlus: () {
-                print('tap plus');
-              }),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-            width: MediaQuery.of(context).size.width * 0.9,
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
             height: 69,
-            child: CustomButton(
-              handledButton: () {},
-              label: "CREAR USUARIO",
-              model: CustomButtonModel(color: CustomButtonColor.dark),
-            )),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
+            child: CustomNavigationBar(
+                model: CustomNavigationBarModel(),
+                onTapPlus: () {
+                  print('tap plus');
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 69,
+              child: CustomButton(
+                handledButton: () {},
+                label: "CREAR USUARIO",
+                model: CustomButtonModel(color: CustomButtonColor.dark),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 150,
+              child: CustomImage(
+                model: CustomImageModel(color: CustomImageColor.dark),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
             width: MediaQuery.of(context).size.width * 0.8,
             height: 150,
-            child: CustomImage(
-              model: CustomImageModel(color: CustomImageColor.dark),
-            )),
-      ]),
+            child: CustomInput(
+              model: CustomInputColorModel(
+                label: "Usuario",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 150,
+            child: CustomInput(
+              model: CustomInputColorModel(
+                label: "Password",
+                color: CustomInputColor.dark,
+                typeInput: CustomInputType.password,
+                isObscure: false,
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
